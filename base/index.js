3330975087649c08eaba669650b3027469b07bb5
shudingbo <shudingbo@qq.com>
2020-10-12 15:08:24 +0800

    application add getCfg function

--- base/index.js
+++ base/index.js
@@ -3,6 +3,30 @@
  * @namespace SexPomelo
  */
 
+ /** 路由Arg
+  * @typedef {Object} RouteArg
+  * @property {number} id - id
+  * @property {number} type - 类型
+  * @property {number} compressRoute - 是否压缩路由
+  * @property {number} route - 路由
+  * @property {number} compressGzip - 是否压缩
+  * @property {object} body - 数据体
+  * @property {number[]} body.data - 数据
+  * @property {string} body.serName - 服务器名
+  */
+
+
+ /** 路由信息
+ * 
+ * @typedef {Object} RouteMsg
+ * @property {string} namespace - 命名空间
+ * @property {string} serverType - 服务类型
+ * @property {string} service - 服务
+ * @property {string} method - 方法
+ * @property {RouteArg[]} args - 参数
+ */
+
+
 /** sex-pomelo application
  * @member {SexPomelo} SexPomelo#Application
  * @since 2.2.15
